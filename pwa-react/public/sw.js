c6bab39842dd4b8e4d0d860582006bccb9c3a239
Canberkkmn <canberkkaman@gmail.com>
2025-03-17 23:54:03 +0300

    improvements

---
+++ pwa-react/public/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "pwa-react-cache";
+const urlsToCache = ["/index.html", "/offline.html"];
+
+const self = this;
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      console.log(response);
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).catch(() => caches.match("/offline.html"));
+    })
+  );
+});
+
+self.addEventListener("active", (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
